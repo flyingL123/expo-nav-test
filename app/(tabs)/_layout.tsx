be7154e483ab16324d5775689e2e889e1e0855c8
Michael Leff <leff.michael@gmail.com>
2024-09-13 22:34:42 -0400

    Update

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -25,74 +25,74 @@ export default function TabLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    // <Tabs
-    //   screenOptions={{
-    //     tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-    //     // Disable the static render of the header on web
-    //     // to prevent a hydration error in React Navigation v6.
-    //     headerShown: useClientOnlyValue(false, true),
-    //   }}>
-    //   <Tabs.Screen
-    //     name="index"
-    //     options={{
-    //       title: 'Tab One',
-    //       tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-    //       headerRight: () => (
-    //         <Link href="/modal" asChild>
-    //           <Pressable>
-    //             {({ pressed }) => (
-    //               <FontAwesome
-    //                 name="info-circle"
-    //                 size={25}
-    //                 color={Colors[colorScheme ?? 'light'].text}
-    //                 style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-    //               />
-    //             )}
-    //           </Pressable>
-    //         </Link>
-    //       ),
-    //     }}
-    //   />
-    //   <Tabs.Screen
-    //     name="two"
-    //     options={{
-    //       title: 'Tab Two',
-    //       tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-    //     }}
-    //   />
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        // Disable the static render of the header on web
+        // to prevent a hydration error in React Navigation v6.
+        headerShown: useClientOnlyValue(false, true),
+      }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Tab One',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+          headerRight: () => (
+            <Link href="/modal" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="info-circle"
+                    size={25}
+                    color={Colors[colorScheme ?? 'light'].text}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="two"
+        options={{
+          title: 'Tab Two',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
       
-    //   <Tabs.Screen
-    //     name="three"
-    //     options={{
-    //       title: 'Tab Three',
-    //       tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
-    //     }}
-    //   />
-    // </Tabs>
+      <Tabs.Screen
+        name="three"
+        options={{
+          title: 'Tab Three',
+          tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
+        }}
+      />
+    </Tabs>
 
     // Uncomment the tabs above, and comment the Tabs below, and the stack in tab 3 will work as expected.
-    <Tab.Navigator>
-      <Tab.Screen
-        name="index"
-        component={TabOne}
-        options={{
-          tabBarIcon: 'home'
-        }}
-      />
-      <Tab.Screen
-        name="two"
-        component={TabTwo}
-        options={{
-          tabBarIcon: 'pound'
-        }}
-      />
-      <Tab.Screen
-        name="three"
-        component={TabThree}
-        options={{
-          tabBarIcon: 'magnify'
-        }}
-      />
-    </Tab.Navigator>
+    // <Tab.Navigator>
+    //   <Tab.Screen
+    //     name="index"
+    //     component={TabOne}
+    //     options={{
+    //       tabBarIcon: 'home'
+    //     }}
+    //   />
+    //   <Tab.Screen
+    //     name="two"
+    //     component={TabTwo}
+    //     options={{
+    //       tabBarIcon: 'pound'
+    //     }}
+    //   />
+    //   <Tab.Screen
+    //     name="three"
+    //     component={TabThree}
+    //     options={{
+    //       tabBarIcon: 'magnify'
+    //     }}
+    //   />
+    // </Tab.Navigator>
   );
 }
